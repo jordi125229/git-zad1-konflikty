250b61ff5b41a81bcbfdd21e5c1d1c1319df59c1
Piotr <piotr.jedraszczyk12@gmail.com>
2026-05-11 18:44:43 +0200

    feat: add basic restaurant menu

---
+++ Menu.java
@@ -0,0 +1,7 @@
+public class Menu {
+    public static void main(String[] args) {
+        System.out.println("=== MENU RESTAURACJI ===");
+        System.out.println("1. Pizza Margherita");
+        System.out.println("2. Burger klasyczny");
+    }
+}
